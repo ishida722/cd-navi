15b4b3903dcb56736e69ac9e45bfeae164f5a093
chu <they.know.who.is.the.strongest@gmail.com>
2017-04-05 17:34:28 +0900

    outside json ok

--- cd-navi.py
+++ cd-navi.py
@@ -1,10 +1,11 @@
 import os
+import sys
 import json
 import posixpath
 
 class Navigator:
     def __init__(self, jsondb):
-        self.db = json.loads(jsondb)
+        self.db = json.load(jsondb)
         self.destination = None
 
     def IsHit(self, word):
@@ -14,70 +15,51 @@ class Navigator:
             return True
         else: return False
 
-def UpCd():
-    os.chdir('..')
+class Driver:
+    def cwd(self):
+        return os.getcwd().replace(os.path.sep, '/')
 
-def ProjectRoot():
-    while():
-        ls = os.listdir('.')
-        if('.svn' in ls): break
-        if('.git' in ls): break
-        UpCd()
-    return os.getcwd()
+    def UpCd(self):
+        os.chdir('..')
 
-json_string = '''
- {
-     "1000":"C1000_*",
-     "sekkei":"C1000_*/0100_*/0130_*",
-     "130":"C1000_*/0100_*/0130_*",
-     "qa":"C1000_*/0300_*/0320_*",
-     "C1000_*/0300_*/0320_*":320,
-     "plan":"C1000_*/0200_*/0220_*",
-     "C1000_*/0200_*/0220_*":220,
-     "yotei":"C1000_*/0200_*/0220_*/DevSched_Soft",
-     "devsched":"C1000_*/0200_*/0220_*/DevSched_Soft",
-     "dsgn":"C1000_*/0400_*",
-     "400":"C1000_*/0400_*",
-     "bug*":"C1000_*/0400_*/0410_*",
-     "410":"C1000_*/0400_*/0410_*",
-     "gizjirok*":"C1000_*/0400_*/0420_*",
-     "420":"C1000_*/0400_*/0420_*",
-     "480":"C1000_*/0400_*/0480_*",
-     "sheet":"C1000_*/0400_*/0480_*",
-     "chkcode":"C1000_*/0400_*/0480_*/CodeRevChkList",
-     "chkintegresult":"C1000_*/0400_*/0480_*/FctnIntegTestResultRevChkList",
-     "chkintegspec":"C1000_*/0400_*/0480_*/FctnIntegTestSpecRevChkList",
-     "warikomi":"C1000_*/0400_*/0480_*/IntrptVerifSh",
-     "intr*":"C1000_*/0400_*/0480_*/IntrptVerifSh",
-     "5k0":"C1000_*/0500_*/05K0_*",
-     "test":"C1000_*/0600_*",
-     "600":"C1000_*/0600_*",
-     "6Kk0":"C1000_*/0600_*/06K0_*",
-     "integ*":"C1000_*/0600_*/06K0_*",
-     "qac":"C1000_*/0600_*/06J0_*/QacCheckResult",
-     "spec":"C2000_*/C2100_*",
-     "2100":"C2000_*/C2100_*",
-     "func*":"C2000_*/C2100_*/C2110_*/0020_*",
-     "20":"C2000_*/C2100_*/C2110_*/0020_*",
-     "2000":"C2000_*",
-     "naibu":"C2000_*/C2100_*/C2120_*",
-     "2120":"C2000_*/C2100_*/C2120_*",
-     "2200":"C2000_*/C2200_*",
-     "manage":"C2000_*/C2200_*",
-     "nittei":"C2000_*/C2200_*/C2210_*",
-     "2210":"C2000_*/C2200_*/C2210_*",
-     "2400":"C2000_*/C2400_*",
-     "rule*":"C2000_*/C2400_*",
-     "housin":"junpToC1000_*/0200_*/0220_*/SoftDsgnPlcyVerifResults"
- }
-'''
+    def GoToProjectRoot(self):
+        while(True):
+            ls = os.listdir('.')
+            if('.svn' in ls): break
+            elif('.git' in ls): break
+            elif('.root' in ls): break
+            self.UpCd()
 
-# ProjectRoot()
-# print(os.getcwd())
-navigator = Navigator(json_string)
-navigator.IsHit('sekkei')
-# desti = os.path.abspath(navigator.destination)
-# print(desti)
-print(navigator.destination)
-print(ProjectRoot())
-print(posixpath.join(os.getcwd().replace(os.path.sep, '/'), "C2000_*/C2400_*" ))
+    def GoToTrueRoot(self):
+        while(True):
+            ls = os.listdir('.')
+            if('.root' in ls): break
+            self.UpCd()
+
+    def MakeTrueRootPath(self):
+        self.GoToTrueRoot()
+        self.trueRoot = self.cwd()
+
+    def __init__(self):
+        self.currentDir = self.cwd()
+        self.GoToProjectRoot()
+        self.projectRoot = self.cwd()
+
+args = sys.argv
+driver = Driver()
+all_path = driver.currentDir
+
+if(len(args) < 2):
+    print(all_path)
+    sys.exit()
+
+db = posixpath.join(driver.projectRoot, '.fcdindex.json')
+json_file = open(db, 'r')
+navigator = Navigator(json_file)
+json_file.close()
+
+if(navigator.IsHit(args[1])):
+    all_path = posixpath.join(driver.projectRoot, navigator.destination)
+
+print(all_path)
+sys.exit()
